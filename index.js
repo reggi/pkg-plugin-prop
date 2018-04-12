8df5d0d10e15b925896a55dd33a9c641b6dd7511
Thomas Reggi <thomas@reggi.com>
2018-04-12 00:58:51 -0400

    only working packages in packages

--- index.js
+++
@@ -1,8 +0,0 @@
-import {reduce, toPairs, set} from 'lodash'
-
-export default ({pkg, opt}) => {
-  if (!opt) return pkg
-  return reduce(toPairs(opt), (acq, pair) => {
-    return set(acq, pair[0], pair[1])
-  }, pkg)
-}
